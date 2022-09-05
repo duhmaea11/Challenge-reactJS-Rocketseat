ef34837986fd4d25c8a26346a666070564386312
Eduardo da Silva <eduardo.silva@mobiauto.com.br>
2022-09-05 11:49:58 -0300

    Change styles

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -49,12 +49,12 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minhas Tasks</h2>
 
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Adicionar novo todo" 
+            placeholder="Adicionar uma nova Task" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
